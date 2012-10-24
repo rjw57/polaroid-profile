eabf7841a28ee3c314a07bb17c81fed2ad8d6e83
Chris Elsmore <elsmorian@Hawk.local>
2012-10-24 11:39:58 +0100

    Added fix to stop browser caching gravatar pictures

--- scripts.js
+++ scripts.js
@@ -3,14 +3,17 @@ function trim (str) {
 }
 
 function gravatar_url(email, size) {
-  var size_query = '';
+  var size_query = '?';
   if(size) {
-    size_query = '?s=' + parseInt(size);
+    size_query += 's=' + parseInt(size)+'&';
   }
+  
+  var d = new Date();
+  var cacheBuster = 'date='+d.getTime().toString()
 
   email = hex_md5(trim(email).toLowerCase());
-
-  return '<img src="https://www.gravatar.com/avatar/' + email + size_query + '">';
+  console.log('<img src="https://www.gravatar.com/avatar/' + email + size_query + cacheBuster + '">');
+  return '<img src="https://www.gravatar.com/avatar/' + email + size_query + cacheBuster + '">';
 }
 
 $(document).ready(function() {
